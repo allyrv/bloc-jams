12dbffb5b49edcf0c6c8838f4a0cddfe36ad5f68
allyrv <allyrvargas@gmail.com>
2017-06-28 18:49:47 -0700

    checkpoint 12 Part I: Done

--- scripts/album.js
+++ scripts/album.js
@@ -26,10 +26,24 @@ var albumMarconi = {
         { title: 'Wrong phone number', duration: '2:15'}
     ]
 };
+var albumLamar = {
+    title: 'DAMN.',
+    artist: 'Kendrick Lamar',
+    label: 'Top Dawg',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'BLOOD.', duration: '1:58' },
+        { title: 'DNA', duration: '3:06' },
+        { title: 'YAH', duration: '2:40'},
+        { title: 'ELEMENT', duration: '2:57' },
+        { title: 'FEEL', duration: '3:15'}
+    ]
+};
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -37,25 +51,51 @@ var createSongRow = function(songNumber, songName, songLength) {
 
      return template;
  };
- var setCurrentAlbum = function(album) {
-      var albumTitle = document.getElementsByClassName('album-view-title')[0];
-      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-      var albumImage = document.getElementsByClassName('album-cover-art')[0];
-      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+ var setCurrentAlbum = function(album) {
       albumTitle.firstChild.nodeValue = album.title;
       albumArtist.firstChild.nodeValue = album.artist;
       albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
       albumImage.setAttribute('src', album.albumArtUrl);
 
       albumSongList.innerHTML = '';
-  
+
       for (var i = 0; i < album.songs.length; i++) {
           albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
       }
   };
 
+  var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+  var songRows = document.getElementsByClassName('album-view-song-item');
+  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
   window.onload = function() {
       setCurrentAlbum(albumPicasso);
+
+      songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+     }
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
+      var albums = [albumPicasso, albumMarconi, albumLamar];
+      var index = 1;
+      albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(album[index]);
+        index++;
+        if (index == albums.length) {
+          index = 0;
+        }
+      });
   };
